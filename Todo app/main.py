d8f8957be3ae39ac9617acff98450a44e42e3eb8
Devanand007 <devanandsrinivasan6322@gmail.com>
2024-02-26 18:57:52 +0530

    changed file code to with context manager

--- Todo app/main.py
+++ Todo app/main.py
@@ -7,19 +7,18 @@ while True:
         case 'add':
             todo = input("Enter a todo: ") + "\n"
 
-            file = open("todos.txt", "r")
-            todos = file.readlines()
-            file.close()
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
 
             todos.append(todo)
 
-            file = open("todos.txt", "w")
-            file.writelines(todos)
-            file.close()
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
+
         case 'show':
-            file = open("todos.txt", "r")
-            todos = file.readlines()
-            file.close()
+
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
 
             for index, item in enumerate(todos):
                 item = item.strip('\n')
@@ -28,11 +27,29 @@ while True:
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
+
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+
             new_todo = input("Enter new todo: ")
-            todos[number] = new_todo
+            todos[number] = new_todo + '\n'
+
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
         case 'complete':
             number = int(input("Number of the todo to complete: "))
-            todos.pop(number)
+
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
+
+            message = f"todo {todo_to_remove} was removed from the list"
+            print(message)
         case 'exit':
             break
 
