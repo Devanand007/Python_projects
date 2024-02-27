acdfc3b47c004c8421280347b44242671eb82b12
Devanand007 <devanandsrinivasan6322@gmail.com>
2024-02-27 15:37:12 +0530

    Remove read and write function and added as module

--- Todo app/main.py
+++ Todo app/main.py
@@ -1,20 +1,5 @@
-def get_todos(filepath="todos.txt"):
-    """
-    Read a text file and return the list of
-    to-do items.
-    """
-    with open(filepath, 'r') as get_file:
-        get_todo = get_file.readlines()
-    return get_todo
-
-
-def write_todos(todos_arg, filepath='todos.txt'):
-    """
-        Write a list of to-do items to a text file.
-    """
-    with open(filepath, "w") as file_local:
-        file_local.writelines(todos_arg)
-
+# from read_and_write import get_todos, write_todos
+import read_and_write as rw
 
 while True:
     # Get user input and strip space chars from it
@@ -24,15 +9,15 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = rw.get_todos()
 
         todos.append(todo + "\n")
 
-        write_todos(todos)
+        rw.write_todos(todos)
 
     elif user_action.startswith("show"):
 
-        todos = get_todos()
+        todos = rw.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -44,12 +29,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos()
+            todos = rw.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos(todos)
+            rw.write_todos(todos)
         except ValueError:
             print("your command is not valid.")
             continue
@@ -58,13 +43,13 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = rw.get_todos()
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(todos)
+            rw.write_todos(todos)
 
             message = f"todo {todo_to_remove} was removed from the list"
             print(message)
---
+++ Todo app/read_and_write.py
@@ -0,0 +1,16 @@
+def get_todos(filepath="todos.txt"):
+    """
+    Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, 'r') as get_file:
+        get_todo = get_file.readlines()
+    return get_todo
+
+
+def write_todos(todos_arg, filepath='todos.txt'):
+    """
+        Write a list of to-do items to a text file.
+    """
+    with open(filepath, "w") as file_local:
+        file_local.writelines(todos_arg)
