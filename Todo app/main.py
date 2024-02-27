2b2366395a683015fa9e4fe4b9089040d8f4d70a
Devanand007 <devanandsrinivasan6322@gmail.com>
2024-02-27 13:36:59 +0530

    added get_todo function  and removed todo readline

--- Todo app/main.py
+++ Todo app/main.py
@@ -1,7 +1,12 @@
-def get_todos():
-    with open('todos.txt', 'r') as get_file:
+def get_todos(filepath):
+    with open(filepath, 'r') as get_file:
         get_todo = get_file.readlines()
     return get_todo
+
+
+def write_todos(filepath, todos_arg):
+    with open(filepath, "w") as file_local:
+        file_local.writelines(todos_arg)
 
 
 while True:
@@ -12,16 +17,15 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = get_todos('todos.txt')
 
         todos.append(todo + "\n")
 
-        with open("todos.txt", "w") as file:
-            file.writelines(todos)
+        write_todos('todos.txt', todos)
 
     elif user_action.startswith("show"):
 
-        todos = get_todos()
+        todos = get_todos('todos.txt')
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -33,13 +37,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos()
+            todos = get_todos('todos.txt')
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+            write_todos('todos.txt', todos)
         except ValueError:
             print("your command is not valid.")
             continue
@@ -48,19 +51,21 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = get_todos('todos.txt')
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            with open("todos.txt", "w") as file:
-                file.writelines(todos)
+            write_todos('todos.txt', todos)
 
             message = f"todo {todo_to_remove} was removed from the list"
             print(message)
         except IndexError:
             print("There is no item with that number")
+            continue
+        except ValueError:
+            print("your command is not valid.")
             continue
 
     elif user_action.startswith("exit"):
