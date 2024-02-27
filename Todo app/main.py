3cb83d80b03476ec2351feb3f21010f285f5d6ac
Devanand007 <devanandsrinivasan6322@gmail.com>
2024-02-27 13:15:29 +0530

    added get_todo function  and removed todo readline

--- Todo app/main.py
+++ Todo app/main.py
@@ -1,3 +1,9 @@
+def get_todos():
+    with open('todos.txt', 'r') as get_file:
+        get_todo = get_file.readlines()
+    return get_todo
+
+
 while True:
     # Get user input and strip space chars from it
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -6,8 +12,7 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         todos.append(todo + "\n")
 
@@ -16,8 +21,7 @@ while True:
 
     elif user_action.startswith("show"):
 
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -29,8 +33,7 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
@@ -45,8 +48,8 @@ while True:
         try:
             number = int(user_action[9:])
 
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+            todos = get_todos()
+
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
