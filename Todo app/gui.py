15bbb79cca11d5a7cf8b1071566622045a1aa118
Devanand007 <devanandsrinivasan6322@gmail.com>
2024-02-27 19:26:11 +0530

    Add gui py

---
+++ Todo app/gui.py
@@ -0,0 +1,10 @@
+import read_and_write as rw
+import PySimpleGUI as Sg
+
+label = Sg.Text("Type in a to-do")
+input_box = Sg.InputText(tooltip="Enter todo")
+add_button = Sg.Button("Add")
+
+window = Sg.Window("My To-Do App", layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
