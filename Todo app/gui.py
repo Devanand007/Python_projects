94153040105838ddce667fdf648e82f0b4400c88
Devanand007 <devanandsrinivasan6322@gmail.com>
2024-02-28 11:47:42 +0530

    change theme, add clock , handle edit and complete error

--- Todo app/gui.py
+++ Todo app/gui.py
@@ -1,23 +1,31 @@
 import read_and_write as rw
 import PySimpleGUI as Sg
+import time
 
+Sg.theme("Black")
+
+clock = Sg.Text('',key='Clock')
 label = Sg.Text("Type in a to-do")
 input_box = Sg.InputText(tooltip="Enter todo", key="todo")
-add_button = Sg.Button("Add")
+add_button = Sg.Button("Add", size=10)
 list_box = Sg.Listbox(values=rw.get_todos(), key='todos',
-                      enable_events=True, size=[45, 10],)
+                      enable_events=True, size=[45, 10])
 edit_button = Sg.Button("Edit")
+complete_button = Sg.Button("Complete")
+exit_button = Sg.Button("Exit")
 
 window = Sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
-                   font=('Helvetica', 20),
+                   layout=[[clock],
+                           [label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
+                   font=('Helvetica', 10),
                    )
 
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['todos'])
+    event, values = window.read(timeout=200)
+    window['Clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
             todos = rw.get_todos()
@@ -26,18 +34,32 @@ while True:
             rw.write_todos(todos)
             window['todos'].update(values=todos)
         case 'Edit':
-            todo_to_edit = values["todos"][0]
-            new_todo = values['todo']
+            try:
+                todo_to_edit = values["todos"][0]
+                new_todo = values['todo']
 
-            todos = rw.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo+"\n"
-            rw.write_todos(todos)
-            window['todos'].update(values=todos)
+                todos = rw.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo+"\n"
+                rw.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                Sg.popup("Please select an item", font=('Helvetica', 10))
+        case "Complete":
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = rw.get_todos()
+                todos.remove(todo_to_complete)
+                rw.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value="")
+            except IndexError:
+                Sg.popup("Please select an item", font=('Helvetica', 10))
 
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
-
         case Sg.WIN_CLOSED:
             break
 window.close()
