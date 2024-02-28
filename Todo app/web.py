a8fdfef293ed0f4e468b7474ac7aea1cf88235fc
Devanand007 <devanandsrinivasan6322@gmail.com>
2024-02-28 13:19:35 +0530

    Implemented " add new todo item"

--- Todo app/web.py
+++ Todo app/web.py
@@ -3,6 +3,13 @@ import read_and_write as rw
 
 todos = rw.get_todos()
 
+
+def add_todo():
+    todo_local = st.session_state['new_todo'] + "\n"
+    todos.append(todo_local)
+    rw.write_todos(todos)
+
+
 st.title("My Todo App")
 st.subheader("This my todo app")
 st.write("this app is to increase your productivity")
@@ -10,4 +17,5 @@ st.write("this app is to increase your productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="",placeholder="Add new todo...")
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
